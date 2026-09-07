1e9b2c1af0bcdf5686aa75e564e4b774acc04668
os-progr <edaninguna@gmail.com>
2026-09-07 10:23:37 -0500

    Actualizacion version 3.1 con Firebase Auth y Google Sign-In nativo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mddorma.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 11
+        versionName = "3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
